3b1642add64c391073db90ff5eb173c6fdf496b5
hamza-ihind <ihind.mizo@gmail.com>
2023-11-05 02:22:32 +0100

    initiated and working in homepage

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,45 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { SignIn, SignedOut } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
+export const metadata = {
+  title: "threads",
+  description: "A Next.js 13 meta Threads Application",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+
+            <SignedOut>
+              <SignIn />
+            </SignedOut>
+
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
